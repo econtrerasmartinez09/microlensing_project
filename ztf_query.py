dc05309b79269d65a937967faf1be65fae0b46f0
Ezequiel Contreras Martinez <109313960+econtrerasmartinez09@users.noreply.github.com>
2023-07-10 13:32:49 -0700

    Update ztf_query.py

--- ztf_query.py
+++ ztf_query.py
@@ -7,10 +7,12 @@ from io import StringIO
 import pandas as pd
 import requests
 
-USER = "USER"
-PASS = "PASS"
+USER = "USER"   # Insert your username
+PASS = "PASS"   # Insert your password
 
-curl --user ${'USER'}:${'PASS'} [...] "https://irsa.ipac.caltech.edu/cgi-bin/ZTF/nph_light_curves?ID=281216400001763"
+# Note: the ID for this specific event is 281216400001763 which is required at the end of the following end 
+
+curl --user myemail@astronomy.edu:mypassword -o out.tbl "https://irsa.ipac.caltech.edu/cgi-bin/ZTF/nph_light_curves?ID=281216400001763"
 
 #wget --auth-no-challenge --user=$USER --pass=$PASS [..] "https://irsa.ipac.caltech.edu/cgi-bin/ZTF/nph_light_curves?ID=281216400001763"
 

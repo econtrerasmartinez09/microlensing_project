4f69ea3b3c139ae05439f26abbaeb35b362fc7d6
Ezequiel Contreras Martinez <109313960+econtrerasmartinez09@users.noreply.github.com>
2023-07-17 11:14:32 -0700

    Delete atlas_query directory

--- atlas_query/atlas_app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class QueryForm(forms.Form):
-	RA  = forms.CharField(label='RA', max_length=100)
-	Dec = forms.CharField(label='Dec', max_length=100)
-	MJD = forms.CharField(label='MJD', max_length=100)
-	Email = forms.CharField(label='Email', max_length=100)
